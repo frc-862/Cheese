332651f0ce0c56db79401529bd8795c6887bdd1f
vilok1 <vgunti777@gmail.com>
2025-11-06 20:45:59 -0500

    generate swerve code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,35 +4,49 @@
 
 package frc.robot;
 
+import frc.robot.Constants.DrivetrainConstants.DriveRequests;
+import frc.robot.Constants.DrivetrainConstants.TunerConstants;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.subsystems.Swerve;
 import frc.util.LightningContainer;
 import frc.util.shuffleboard.LightningShuffleboard;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class RobotContainer extends LightningContainer {
 
     private SendableChooser<Command> autoChooser;
+    private Swerve drivetrain;
+    private XboxController driver = new XboxController(ControllerConstants.DRIVER_PORT);
  
     @Override
-    protected void initializeSubsystems() {}
+    protected void initializeSubsystems() {
+        drivetrain = TunerConstants.createDrivetrain();
+    }
 
     @Override
-    protected void configureDefaultCommands() {}
+    protected void configureDefaultCommands() {
+        drivetrain.setDefaultCommand(drivetrain.applyRequest(DriveRequests.getDrive(() -> -driver.getLeftY(),
+                () -> -driver.getLeftX(), () -> -driver.getRightX())));
+    }
 
     @Override
     protected void configureButtonBindings() {}
 
     @Override
     protected void initializeNamedCommands() {
-        autoChooser = AutoBuilder.buildAutoChooser();
-        LightningShuffleboard.send("Auton", "Auto Chooser", autoChooser);
+        // autoChooser = AutoBuilder.buildAutoChooser();
+        // LightningShuffleboard.send("Auton", "Auto Chooser", autoChooser);
     }
 
     @Override
     protected Command getAutonomousCommand() {
-        return autoChooser.getSelected();
+        // return autoChooser.getSelected();
+        return new InstantCommand();
     }
 }
