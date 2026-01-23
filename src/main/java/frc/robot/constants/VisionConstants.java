51b833e2bca8041a2f4f6ce034ecc04e5349f141
Zan e1 <zbeidas@icloud.com>
2026-01-22 19:54:16 -0500

    For vision

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -4,16 +4,14 @@ import java.util.List;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import static edu.wpi.first.units.Units.Inches;
 
 public class VisionConstants {
 
     public static final List<Short> TAG_IGNORE_LIST = List.of();
 
     // TODO: Update this to be the correect field layout for this season,
-    public static final AprilTagFieldLayout DEFAULT_TAG_LAYOUT = AprilTagFieldLayout
+    public static final AprilTagFieldLayout REBUILT_FIELD = AprilTagFieldLayout
             .loadField(AprilTagFields.k2026RebuiltWelded);
 
     public static final double POSE_AMBIGUITY_TOLERANCE = 1;
@@ -24,17 +22,20 @@ public class VisionConstants {
     public static final CameraConstant[] CAMERA_CONSTANTS = new CameraConstant[] {
         new CameraConstant("leftCam", 
             new Transform3d(
-                Inches.of(11.25),
-                Inches.of(-11.25),   // RIGHT side
-                Inches.of(10.5),
-                new Rotation3d(0, 15, -45))),
+                // Inches.of(11.25),
+                // Inches.of(-11.25),   // RIGHT side
+                // Inches.of(10.5),
+                // new Rotation3d(0, 15, -45)
+            )),
 
         new CameraConstant("rightCam",
             new Transform3d(
-                Inches.of(11.25),
-                Inches.of(11.25),    // LEFT side
-                Inches.of(10.5),
-                new Rotation3d(0, 15, 45))),
+                // Inches.of(11.25),
+                // Inches.of(11.25),    // LEFT side
+                // Inches.of(10.5),
+                // new Rotation3d(0, 15, 45)
+                )
+            ),
     };
 
 }
--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -4,49 +4,32 @@
 
 package frc.robot.subsystems;
 
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
 import com.ctre.phoenix6.Utils;
 
-import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.VecBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructSubscriber;
 import edu.wpi.first.wpilibj.DataLogManager;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.VisionConstants;
-import frc.util.shuffleboard.LightningShuffleboard;
 
 public class PhotonVision extends SubsystemBase {
-    private record VisionInfo(PhotonPipelineResult result, EstimatedRobotPose pose) {};
+    private record VisionInfo(double timestamp, double ambiguity, Pose2d pose) {};
 
     // The drivetrain to add vision measurments
     Swerve drivetrain;
-
-    // mac
-    MacMini mac;
 
     // Atomic
     AtomicReference<VisionInfo> pose;
 
     // executor
     ScheduledExecutorService executor1;
-    ExecutorService executor2;
-
 
     /** Creates a new PhotonVision.
      * 
@@ -54,27 +37,57 @@ public class PhotonVision extends SubsystemBase {
      */
     public PhotonVision(Swerve drivetrain) {
         this.drivetrain = drivetrain;
-        mac = new MacMini();
 
         executor1 = Executors.newSingleThreadScheduledExecutor();
-        executor2 = Executors.newSingleThreadExecutor();
 
         pose = new AtomicReference<>(null);
 
-        LightningShuffleboard.setPose2d("vision", "vision_pose", new Pose2d());
+        NetworkTableInstance nt = NetworkTableInstance.getDefault();
+        StructSubscriber<Pose2d> poseSubscriber = nt.getTable("Mac").getStructTopic("estimated_pose", Pose2d.struct).subscribe(new Pose2d());
+        DoubleSubscriber ambiguitySubscriber = nt.getTable("Mac").getDoubleTopic("pose_ambiguity").subscribe(1);
+        DoubleSubscriber timestampSubscriber = nt.getTable("Mac").getDoubleTopic("pose_ambiguity").subscribe(-1);
 
         executor1.schedule(() -> {
             while (true) { 
-                try {
-                    VisionInfo result = executor2.submit(() -> mac.getEstimatedPose()).get();
-                    if (result != null) {
-                        pose.set(result);
+                Pose2d localPose = null;
+                double ambiguity = 1;
+                double timestamp = -1;
+
+                if (poseSubscriber.exists()) {
+                    Pose2d value = poseSubscriber.get();
+                    if (value == null) {
+                        pose.set(null);
+                        continue;
                     }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    log("Failed to get data");
+
+                    localPose = value;
                 }
+
+                if (ambiguitySubscriber.exists()) {
+                    double value = ambiguitySubscriber.getAsDouble();
+                    
+                    if (value == 1) {
+                        pose.set(null);
+                        continue;
+                    }
+
+                    ambiguity = value;
                 }
+
+                if (timestampSubscriber.exists()) {
+                    double value = timestampSubscriber.getAsDouble();
+
+                    if (value < 0) {
+                        pose.set(null);
+                        continue;
+                    }
+
+                    timestamp = value;
+                }
+
+                pose.set(new VisionInfo(timestamp, ambiguity, localPose));
+            }
+            
         }, 0, TimeUnit.MILLISECONDS);
     }
  
@@ -83,197 +96,13 @@ public class PhotonVision extends SubsystemBase {
         if (pose.get() != null) {
             VisionInfo updatedPose = pose.getAndSet(null);
 
-            double bestTagAmbiguity = updatedPose.result.getBestTarget().poseAmbiguity;
-             LightningShuffleboard.setPose2d("vision", "vision_pose", updatedPose.pose.estimatedPose.toPose2d());
+            double bestTagAmbiguity = updatedPose.ambiguity();
+            
             drivetrain.addVisionMeasurement(
-                updatedPose.pose.estimatedPose.toPose2d(), 
-                Utils.fpgaToCurrentTime(updatedPose.pose.timestampSeconds), 
+                updatedPose.pose(), 
+                Utils.fpgaToCurrentTime(updatedPose.timestamp), 
                 VecBuilder.fill(bestTagAmbiguity, bestTagAmbiguity, bestTagAmbiguity));
             log("Added vision measurment");
-        }
-    }
-
-    // -------------------------------------
-
-    private class MacMini {
-        // Camera info
-        private record CameraInfo(PhotonCamera camera, PhotonPoseEstimator poseEstimator) {}; 
-        // private record VisionInfo(PhotonPipelineResult result, EstimatedRobotPose pose) {};
-
-        // Cameras
-        CameraInfo[] cameras;
-
-        MacMini() {            
-            this.cameras = new CameraInfo[VisionConstants.CAMERA_CONSTANTS.length];
-            
-            // Create the cameras
-            for (int i = 0; i < VisionConstants.CAMERA_CONSTANTS.length; i++) {
-                AprilTagFieldLayout fieldLayout;
-
-                try {
-                    // Get the path to the field from the deploy directory
-                    Path fieldPath = Filesystem.getDeployDirectory()
-                        .toPath()
-                        .resolve("field0120.json");
-
-                    fieldLayout = new AprilTagFieldLayout(fieldPath);
-                } catch (Exception e) {
-                    // Just use the default field if we can't get it
-                    DataLogManager.log("[PHOTON VISION] Can't load field resource-- using default field");
-                    fieldLayout = VisionConstants.DEFAULT_TAG_LAYOUT;
-                }
-
-                // Get the pose estimator
-                PhotonPoseEstimator poseEstimator =
-                        new PhotonPoseEstimator(
-                                fieldLayout,
-                                VisionConstants.CAMERA_CONSTANTS[i].offset()
-                        );
-                    
-                // Create the camera using the name from our constant
-                PhotonCamera camera = new PhotonCamera(VisionConstants.CAMERA_CONSTANTS[i].name());
-
-                // Create the camera
-                this.cameras[i] = new CameraInfo(camera, poseEstimator);
-                log(VisionConstants.CAMERA_CONSTANTS[i].name() + " info created sucessfully");
-            }
-
-        }
-
-        public VisionInfo getEstimatedPose() {
-            if (cameras == null || cameras.length == 0) {
-                log("No cameras configured");
-                return null;
-            }
-
-            try {
-                VisionInfo[] poses = new VisionInfo[VisionConstants.CAMERA_CONSTANTS.length];
-                
-                for (int i = 0; i < VisionConstants.CAMERA_CONSTANTS.length; i++) {
-                    poses[i] = getVisionPose(cameras[i]);
-                }
-
-                return getBestPose(poses);
-            } catch (Exception e) {
-                log("Failed to get pose");
-                e.printStackTrace();
-                return null;
-            }
-        }
-
-        // Gets the latest result from multiple results
-        private PhotonPipelineResult getLatestResult(List<PhotonPipelineResult> results) {
-            int latestResultIndex = 0;
-
-            for (int i = 0; i < results.size(); i++) {
-                if (results.get(i).getTimestampSeconds() > results.get(latestResultIndex).getTimestampSeconds()) {
-                    latestResultIndex = i;
-                }
-            }
-
-            PhotonPipelineResult latestResult = results.get(latestResultIndex);
-
-            return latestResult;
-        }
-
-        // This will get the best pose
-        private VisionInfo getBestPose(VisionInfo[] visionInfos) {
-            VisionInfo bestPose = null;
-
-            for (VisionInfo info : visionInfos) {
-                if (info == null) {
-                    continue;
-                }
-                
-                if (bestPose == null) {
-                    bestPose = info;
-                    continue;
-                }
-
-                if (bestPose.result().getBestTarget().poseAmbiguity > info.result().getBestTarget().poseAmbiguity) {
-                    bestPose = info;
-                }
-            }
-            log("Got best pose");
-            return bestPose;
-        }
-
-        private VisionInfo getVisionPose(CameraInfo cameraInfo) {
-            PhotonCamera camera = cameraInfo.camera;
-
-            List<PhotonPipelineResult> results = camera.getAllUnreadResults();
-
-            // If theres no results just skip this iteration
-            if (results.isEmpty()) {
-                log(cameraInfo.camera.getName() + "'s Result is null");
-                return null;
-            }
-            
-            // Get the latest result of all thme
-            PhotonPipelineResult latestResult = getLatestResult(results);
-
-            // Filter out the targets
-            List<PhotonTrackedTarget> filteredTargets = new ArrayList<>(latestResult.getTargets());
-            filteredTargets.removeIf((tag) -> VisionConstants.TAG_IGNORE_LIST.contains((short) tag.getFiducialId()));
-
-            // Scrap it if the new result has no target
-            if (filteredTargets.isEmpty()) {
-                return null;
-            }
-
-            PhotonPipelineResult useableResult = latestResult;
-
-            // Create a new result to use -- Using the same metadata as the original latest result
-            if (!latestResult.getTargets().stream().allMatch((target) -> filteredTargets.contains(target))) {
-                useableResult = new PhotonPipelineResult(
-                    latestResult.metadata,
-                    filteredTargets,
-                    Optional.empty()
-                );
-            }
-
-            // If pose ambiguity is to high well scrap the result
-            boolean highPoseAmbiguity = latestResult.getBestTarget().getPoseAmbiguity() > VisionConstants.POSE_AMBIGUITY_TOLERANCE;
-
-            // If the best tag's distance is too far than scrap the result
-            double bestDistance = useableResult.getBestTarget().getBestCameraToTarget().getTranslation().getNorm();
-            boolean highDistance = bestDistance > VisionConstants.TAG_DISTANCE_TOLERANCE;
-
-            // If we have high ambiguity or high distance then just return back a null/"empty" value
-            if (highPoseAmbiguity || highDistance) {
-                return null;
-            }
-            
-            // Get the estimated position
-            Optional<EstimatedRobotPose> poseOpt = cameraInfo.poseEstimator().estimateCoprocMultiTagPose(useableResult);
-
-            // If the estimated position is there run this code
-            if (poseOpt.isPresent()) {
-                // The pose
-                EstimatedRobotPose pose = poseOpt.get();
-                
-                log("Used multitag result");
-
-                // Add the vision measurment
-                return new VisionInfo(useableResult, pose);
-            } else {
-                // Get the estimated position
-                poseOpt = cameraInfo.poseEstimator().estimateLowestAmbiguityPose(useableResult);
-                
-                if (poseOpt.isPresent()) {
-                    // The pose
-                    EstimatedRobotPose pose = poseOpt.get();
-
-                    log("Used singletag result");
-
-                    // Add the vision measurment
-                    return new VisionInfo(useableResult, pose);
-                }
-            }
-
-            // We have no pose if were here
-            log("No pose");
-            return null;
         }
     }
 
